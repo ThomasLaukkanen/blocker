68489f51cc33ccd4afeb18a7855be517507c65fa
Thomas Laukkanen <thomas.laukkanen@etraveligroup.com>
2023-09-28 19:21:07 +0200

    remove tabs

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.getElementById("save").addEventListener("click", function () {
   chrome.storage.local.set({ blockedSites: sites });
   document.getElementById(
     "message"
-  ).innerHTML = `${sites.length} sites are now blocked!`;
+  ).innerHTML = `${sites.length} websites are now blocked!`;
 });
 
 document.getElementById("sitesList").addEventListener("input", function () {
@@ -14,14 +14,16 @@ document.getElementById("sitesList").addEventListener("input", function () {
     .getElementById("sitesList")
     .value.split("\n")
     .filter(Boolean);
-  document.getElementById("message").innerHTML = `Block ${sites.length} sites `;
+  document.getElementById(
+    "message"
+  ).innerHTML = `Block ${sites.length} websites `;
 });
 
 async function getBlockedSites() {
   const { blockedSites } = await chrome.storage.local.get(["blockedSites"]);
   document.getElementById(
     "message"
-  ).innerHTML = `You have blocked ${blockedSites.length} sites`;
+  ).innerHTML = `You have blocked ${blockedSites.length} websites`;
   blockedSites.forEach((site) => {
     document.getElementById("sitesList").value += site + "\n";
   });
